d9dca62709a3aac3155b28ea661a0042cf8c0410
Tran Duc Manh <tdmanh1510@gmail.com>
2025-11-09 11:53:27 +0700

    .

--- app/app.py
+++ app/app.py
@@ -1,12 +1,15 @@
 from flask import Flask, request, make_response, render_template
+import socket
 
 app = Flask(__name__)
 
-USERS = {"alice": "1234", "bob": "abcd"}
+USERS = {"manh": "1234"}
+
 
 @app.route("/", methods=["GET"])
 def index():
-    return render_template("index.html")
+    return render_template("index.html", hostname=socket.gethostname())
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -19,6 +22,7 @@ def login():
             return resp
         return render_template("login.html", error="Invalid username or password")
     return render_template("login.html", error=None)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
